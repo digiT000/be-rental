bf3f9d82bbbef6bde5efe10e94f4b9eb43e9a235
digiT000 <fareldeksano000@gmail.com>
2026-03-07 22:19:19 +0700

    fix import file

--- src/dto/brand/index.ts
+++ src/dto/brand/index.ts
@@ -1,6 +1,6 @@
 // This file serves as an index for all the DTOs related to the Brand entity. It re-exports the types from the individual DTO files, allowing for cleaner and more organized imports in other parts of the application.
-export type { CreateBrandRequest } from "./create-request.dto";
-export type { UpdateBrandRequest } from "./update-request.dto";
-export type { BrandResponse } from "./brand-response.dto";
+export type { CreateBrandRequest } from "./create-request.dto.js";
+export type { UpdateBrandRequest } from "./update-request.dto.js";
+export type { BrandResponse } from "./brand-response.dto.js";
 
-export { toBrandResponse } from "./mapper";
+export { toBrandResponse } from "./mapper.js";
